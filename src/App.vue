ed19af3c758d1027915341d2ca2ffb466e6817ce
EnricoCirilli <enrico.cirilli@gmail.com>
2023-11-27 16:22:52 +0100

    add: commit 1 creating cards

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,26 @@
 import axios from 'axios';
 
 export default{
-  created()
+  data(){
+    return{
+      characters: [],
+    };
+  },
+  created(){
+    //chiamata api delle cards
+    axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0").then((resp) => {
+   console.log(resp);
+   this.characters = resp.data.data;
+    });
+  },
 };
 </script>
 
 <template>
- 
+ <h1>HELLO</h1>
+ <ul>
+  <li v-for="character in characters">{{  character.name }}</li>
+ </ul>
 </template>
 
 <style lang="scss">
